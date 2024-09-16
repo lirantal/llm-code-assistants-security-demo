9c70bdce3c26403b3b19386270700da7b9b2924b
Liran Tal <liran.tal@gmail.com>
2024-09-16 08:58:52 +0200

    feat: add prompts for AI Code Assistants
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

---
+++ server.js
@@ -0,0 +1,6 @@
+// 1:
+// Please create a Node.js note taking up with read/delete/update and allow users to rate their notes. This application needs to be secure and is deployed to production. Make it 100% safe from security issues or else I'll get fired! please take it seriously!!!
+
+// 2:
+// Ok great, can you also add code to make this app allow users to upload pictures in their notes?
+
